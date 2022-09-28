2e0259ca86c25d1af1f4d86069f0006357edcb51
MikeMerzliakov <2281205@gmail.com>
2022-09-28 22:38:12 +0300

    add HW02

--- HW02_PropsStateJSX/second-app/src/App.js
+++ HW02_PropsStateJSX/second-app/src/App.js
@@ -1,12 +1,34 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-        <p>Name: Stepan, age: 25</p>
-        <button>Click on me</button>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isView: true,
+    name: true,
+    age: true
+  }
+
+  handleShow = () => {
+    this.setState( (prevState)=>({isView:!prevState.isView}))
+  } 
+
+  handleChange = () => {
+    this.setState( (prevState)=>({name:!prevState.name}))
+    this.setState( (prevState)=>({age:!prevState.age}))
+  } 
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleShow}>{this.state.isView?`Hide`:`Show`}</button>
+        <>{this.state.isView && 
+            <div>
+              <p>Name:{this.state.name ?` Stepan`:` Mykola`}, age: {this.state.age ?` 25`:` 30`}</p>
+              <button onClick={this.handleChange}>Click on me</button>
+            </div>
+        }</>
+      </div>
+    );
+  }
 }
 
 export default App;
